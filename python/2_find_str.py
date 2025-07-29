5d5d96a1f672dc7a8f3cf08fc0afeca5c5292b3b
Pavel Lavrovskiy <plavrovskiy@yandex-team.ru>
2025-07-29 14:21:21 +0300

    1

--- python/2_find_str.py
+++ python/2_find_str.py
@@ -81,7 +81,7 @@ def parse_dat_file(input_path, output_path):
                 index += 2
 
     # Сортируем строки по полю original
-    strings_sorted = sorted(strings, key=lambda x: int(x['size'], 16), reverse=True)
+    strings_sorted = sorted(strings, key=lambda x: x['original'])
     
     print(f"Найдено строк: {len(strings_sorted)}")
     
